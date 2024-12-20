8d976b628fccac684223a85f081b18ed131353d6
Reem Mohamed <reem1619123@gmail.com>
2024-12-20 22:05:37 +0200

    handel small screens message (till now)

--- src/App.js
+++ src/App.js
@@ -17,29 +17,37 @@ import NotFoundPage from './pages/NotFoundPage.jsx';
 function App() {
   return (
     <div className="App">
+      <div className="desktop">
 
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="*" element={<NotFoundPage />} />
 
-          {/* <Route path="/products" element={<Products />} />
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="*" element={<NotFoundPage />} />
+
+            {/* <Route path="/products" element={<Products />} />
           
           <Route path="/cart" element={<Cart />} />
           
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/profile" element={<Profile />} /> */}
-        </Routes>
-      </Router>
-      <Footer />
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
+      <div className="large-screen-only">
+        <img src={process.env.PUBLIC_URL + "/imgs/not-found.png"} alt="404" />
+        <h1>Sorry, this site is only available on large screens.</h1>
+      </div>
+
     </div>
   );
 }
